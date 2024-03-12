5ee536d2f27c469c85030c8705ede257d5fcad1a
purkylin <purkylin@glowing.com>
2024-03-12 13:21:38 +0800

    Initial Commit

---
+++ libprisma/libprisma.h
@@ -0,0 +1,18 @@
+//
+//  libprisma.h
+//  libprisma
+//
+//  Created by Purkylin King on 2024/3/12.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for libprisma.
+FOUNDATION_EXPORT double libprismaVersionNumber;
+
+//! Project version string for libprisma.
+FOUNDATION_EXPORT const unsigned char libprismaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <libprisma/PublicHeader.h>
+
+
